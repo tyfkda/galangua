8588a7a36dce234af2348b4c28ad04978d1b5440
tyfkda <tyfkda@gmail.com>
2020-08-30 13:27:20 +0900

    Create buffer to rearrange enemy order

--- galangua-core/src/app/game/enemy/appearance_manager.rs
+++ galangua-core/src/app/game/enemy/appearance_manager.rs
@@ -1,17 +1,39 @@
 use super::appearance_table::*;
-use super::enemy::{Enemy, EnemyState};
+use super::enemy::{Enemy, EnemyState, EnemyType};
 use super::traj::Traj;
+use super::traj_command::TrajCommand;
+use super::FormationIndex;
 
+use crate::app::util::unsafe_util::extend_lifetime;
 use crate::framework::types::{Vec2I, ZERO_VEC};
 use crate::util::math::ONE;
+
+#[derive(Debug)]
+struct Info {
+    time: u32,
+    enemy_type: EnemyType,
+    fi: FormationIndex,
+    offset: Vec2I,
+    flip_x: bool,
+    traj_table: &'static [TrajCommand],
+}
+
+impl Info {
+    pub fn new(time: u32, enemy_type: EnemyType, fi: FormationIndex, offset: Vec2I, flip_x: bool,
+               traj_table: &'static [TrajCommand]) -> Self {
+        Self { time, enemy_type, fi, offset, flip_x, traj_table }
+    }
+}
 
 pub struct AppearanceManager {
     stage: u32,
     wait_stationary: bool,
     wait: u32,
     unit: u32,
-    count: u32,
+    time: u32,
     pub(super) done: bool,
+    orders: Vec<Info>,
+    orders_ptr: &'static [Info],
 }
 
 impl AppearanceManager {
@@ -21,14 +43,17 @@ impl AppearanceManager {
             wait_stationary: false,
             wait: 0,
             unit: 0,
-            count: 0,
+            time: 0,
             done: true,
+            orders: Vec::new(),
+            orders_ptr: &[],
         }
     }
 
     pub fn restart(&mut self, stage: u32) {
         *self = Self::new(stage);
         self.done = false;
+        self.orders.clear();
     }
 
     pub fn update(&mut self, enemies: &[Option<Enemy>]) -> Option<Vec<Enemy>> {
@@ -55,130 +80,119 @@ impl AppearanceManager {
             return None;
         }
 
+        if self.orders.is_empty() {
+            self.set_orders();
+            // orders is owned by vec, so it lives as long as self and not worry about that.
+            self.orders_ptr = unsafe { extend_lifetime(&self.orders) };
+
+            self.time = 0;
+        }
+
+        if self.orders_ptr[0].time < self.time {
+            return None
+        }
+
         let mut new_borns = Vec::new();
-        let base = (self.unit * 8) as usize;
+        while self.orders_ptr[0].time == self.time {
+            let p = &self.orders_ptr[0];
+            let mut enemy = Enemy::new(p.enemy_type, &ZERO_VEC, 0, 0);
+
+            let traj = Traj::new(p.traj_table, &p.offset, p.flip_x, p.fi);
+            enemy.set_traj(traj);
+            enemy.formation_index = p.fi;
+
+            new_borns.push(enemy);
+
+            self.orders_ptr = &self.orders_ptr[1..];
+            if self.orders_ptr.is_empty() {
+                break;
+            }
+        }
+
+        self.time += 1;
+        if self.orders_ptr.is_empty() {
+            self.orders_ptr = &[];
+            self.orders.clear();
+
+            self.unit += 1;
+            self.wait_stationary = true;
+            self.wait = 10;
+            self.time = 0;
+        }
+
+        Some(new_borns)
+    }
+
+    fn set_orders(&mut self) {
+        let base = self.unit * 8;
         let entry = &UNIT_TABLE[(self.stage as usize) % UNIT_TABLE.len()][self.unit as usize];
+        let enemy_types = &ENEMY_TYPE_TABLE[(self.unit * 2) as usize ..
+                                            (self.unit * 2) as usize + 2];
+
         match entry.pat {
             0 => {
                 let flip = if entry.flip_x { 1 } else { 0 };
-                {
-                    let index = ORDER[base + (self.count + flip * 4) as usize];
-                    let enemy_type = ENEMY_TYPE_TABLE[(self.unit * 2 + flip) as usize];
-                    let mut enemy = Enemy::new(enemy_type, &ZERO_VEC, 0, 0);
-
-                    let traj = Traj::new(entry.table, &Vec2I::new(8 * ONE, 0), true, index);
-                    enemy.set_traj(traj);
-                    enemy.formation_index = index;
-
-                    new_borns.push(enemy);
-                }
-                {
-                    let index = ORDER[base + (self.count + (1 - flip) * 4) as usize];
-                    let enemy_type = ENEMY_TYPE_TABLE[(self.unit * 2 + (1 - flip)) as usize];
-                    let mut enemy = Enemy::new(enemy_type, &ZERO_VEC, 0, 0);
-
-                    let traj = Traj::new(entry.table, &Vec2I::new(8 * ONE, 0), false, index);
-                    enemy.set_traj(traj);
-                    enemy.formation_index = index;
-
-                    new_borns.push(enemy);
-                }
-
-                self.wait = 16 / 3;
-                self.count += 1;
-                if self.count >= 4 {
-                    self.unit += 1;
-                    self.wait_stationary = true;
-                    self.wait = 10;
-                    self.count = 0;
+                let mut time = 0;
+                for count in 0..4 {
+                    {
+                        let fi = ORDER[(base + (count + flip * 4)) as usize];
+                        let enemy_type = enemy_types[flip as usize];
+                        self.orders.push(Info::new(time, enemy_type, fi, Vec2I::new(8 * ONE, 0),
+                                                   true, entry.table))
+                    }
+                    {
+                        let fi = ORDER[(base + (count + (1 - flip) * 4)) as usize];
+                        let enemy_type = enemy_types[(1 - flip) as usize];
+                        self.orders.push(Info::new(time, enemy_type, fi, Vec2I::new(8 * ONE, 0),
+                                                   false,entry.table));
+                    }
+                    time += 16 / 3;
                 }
             }
             1 => {
-                {
-                    let index = ORDER[base + (self.count / 2 + (self.count & 1) * 4) as usize];
-                    let enemy_type = ENEMY_TYPE_TABLE[(self.unit * 2 + (self.count & 1)) as usize];
-                    let mut enemy = Enemy::new(enemy_type, &ZERO_VEC, 0, 0);
-
-                    let traj = Traj::new(entry.table, &Vec2I::new(8 * ONE, 0), entry.flip_x, index);
-                    enemy.set_traj(traj);
-                    enemy.formation_index = index;
-
-                    new_borns.push(enemy);
-                }
-
-                self.wait = 16 / 3;
-                self.count += 1;
-                if self.count >= 8 {
-                    self.unit += 1;
-                    self.wait_stationary = true;
-                    self.wait = 10;
-                    self.count = 0;
+                let mut time = 0;
+                for count in 0..8 {
+                    let fi = ORDER[(base + (count / 2 + (count & 1) * 4)) as usize];
+                    let enemy_type = enemy_types[(count & 1) as usize];
+                    self.orders.push(Info::new(time, enemy_type, fi, Vec2I::new(8 * ONE, 0),
+                                               entry.flip_x, entry.table));
+                    time += 16 / 3;
                 }
             }
             2 => {
-                {
-                    let index = ORDER[base + self.count as usize];
-                    let enemy_type = ENEMY_TYPE_TABLE[(self.unit * 2 + (self.count & 1)) as usize];
-                    let mut enemy = Enemy::new(enemy_type, &ZERO_VEC, 0, 0);
-
-                    let traj = Traj::new(entry.table, &ZERO_VEC, entry.flip_x, index);
-                    enemy.set_traj(traj);
-                    enemy.formation_index = index;
-
-                    new_borns.push(enemy);
-                }
-
-                self.wait = 16 / 3;
-                self.count += 1;
-                if self.count >= 8 {
-                    self.unit += 1;
-                    self.wait_stationary = true;
-                    self.wait = 10;
-                    self.count = 0;
+                let mut time = 0;
+                for count in 0..8 {
+                    let fi = ORDER[(base + count) as usize];
+                    let enemy_type = enemy_types[(count & 1) as usize];
+                    self.orders.push(Info::new(time, enemy_type, fi, ZERO_VEC, entry.flip_x,
+                                               entry.table));
+                    time += 16 / 3;
                 }
             }
             3 => {
                 let flip = if entry.flip_x { 1 } else { 0 };
-                {
-                    let index = ORDER[base + (self.count + flip * 4) as usize];
-                    let enemy_type = ENEMY_TYPE_TABLE[(self.unit * 2 + flip) as usize];
-                    let mut enemy = Enemy::new(enemy_type, &ZERO_VEC, 0, 0);
-
-                    let traj = Traj::new(entry.table, &Vec2I::new(8 * ONE, 0), entry.flip_x, index);
-                    enemy.set_traj(traj);
-                    enemy.formation_index = index;
-
-                    new_borns.push(enemy);
-                }
-                {
-                    let index = ORDER[base + (self.count + (1 - flip) * 4) as usize];
-                    let enemy_type = ENEMY_TYPE_TABLE[(self.unit * 2 + (1 - flip)) as usize];
-                    let mut enemy = Enemy::new(enemy_type, &ZERO_VEC, 0, 0);
-
-                    let traj = Traj::new(entry.table, &Vec2I::new(-8 * ONE, 0), entry.flip_x, index);
-                    enemy.set_traj(traj);
-                    enemy.formation_index = index;
-
-                    new_borns.push(enemy);
-                }
-
-                self.wait = 16 / 3;
-                self.count += 1;
-                if self.count >= 4 {
-                    self.unit += 1;
-                    self.wait_stationary = true;
-                    self.wait = 10;
-                    self.count = 0;
+                let mut time = 0;
+                for count in 0..4 {
+                    {
+                        let fi = ORDER[(base + (count + flip * 4)) as usize];
+                        let enemy_type = enemy_types[flip as usize];
+                        self.orders.push(Info::new(time, enemy_type, fi, Vec2I::new(8 * ONE, 0),
+                                                   entry.flip_x, entry.table));
+                    }
+                    {
+                        let fi = ORDER[(base + (count + (1 - flip) * 4)) as usize];
+                        let enemy_type = enemy_types[(1 - flip) as usize];
+                        self.orders.push(Info::new(time, enemy_type, fi, Vec2I::new(-8 * ONE, 0),
+                                                   entry.flip_x, entry.table));
+                    }
+                    time += 16 / 3;
                 }
             }
 
             _ => {
                 self.done = true;
-                return None;
             }
         }
-
-        Some(new_borns)
     }
 
     fn is_stationary(&self, enemies: &[Option<Enemy>]) -> bool {
--- galangua-core/src/app/util/unsafe_util.rs
+++ galangua-core/src/app/util/unsafe_util.rs
@@ -2,6 +2,6 @@ pub unsafe fn peep<'a, T: ?Sized>(t: &T) -> &'a mut T {
     &mut *(t as *const T as *mut T)
 }
 
-pub unsafe fn extend_lifetime<'a, T>(t: &'a T) -> &'static T {
+pub unsafe fn extend_lifetime<'a, T: ?Sized>(t: &'a T) -> &'static T {
     std::mem::transmute::<&T, &'static T>(t)
 }
