a662ce335140f5493a175bb81f96db243fa3a172
tyfkda <tyfkda@gmail.com>
2023-03-14 11:25:05 +0900

    Update copy-webpack-plugin

--- galangua-wasm/www/webpack.config.js
+++ galangua-wasm/www/webpack.config.js
@@ -1,15 +1,23 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
+const distPath = path.resolve(__dirname, "dist")
+
 module.exports = {
   entry: "./bootstrap.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: distPath,
     filename: "bootstrap.js",
   },
   mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html', 'default.css', 'assets/**/*'])
+    new CopyWebpackPlugin({
+      patterns: [
+        {from: 'index.html', to: distPath},
+        {from: 'default.css', to: distPath},
+        {from: 'assets/**/*', to: distPath},
+      ],
+    })
   ],
   experiments: {
     syncWebAssembly: true,
