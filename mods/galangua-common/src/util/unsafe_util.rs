3ec99b3b2e1ef11250c5711a3c94ea7da4ead481
tyfkda <tyfkda@gmail.com>
2023-03-20 10:26:59 +0900

    Avoid undefined behavior
    Clippy warns:
    
    ```
     --> mods/galangua-common/src/util/unsafe_util.rs:2:10
      |
    2 |     &mut *(t as *const T as *mut T)
      |          ^^^^^^^^^^^^^^^^^^^^^^^^^^
      |
      = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#cast_ref_to_mut
    ```
    
    [UnsafeCell in std::cell - Rust](https://doc.rust-lang.org/nightly/std/cell/struct.UnsafeCell.html)

--- mods/galangua-common/src/util/unsafe_util.rs
+++ mods/galangua-common/src/util/unsafe_util.rs
@@ -1,7 +1,23 @@
-pub unsafe fn peep<'a, T: ?Sized>(t: &T) -> &'a mut T {
-    &mut *(t as *const T as *mut T)
+use std::cell::UnsafeCell;
+
+pub unsafe fn peep<'a, T>(t: &T) -> &'a mut T {
+    get_mut(get_shared(t))
 }
 
 pub unsafe fn extend_lifetime<'a, T: ?Sized>(t: &'a T) -> &'static T {
     std::mem::transmute::<&T, &'static T>(t)
+}
+
+// Taken from "Memory layout" in [UnsafeCell in std::cell - Rust](https://doc.rust-lang.org/nightly/std/cell/struct.UnsafeCell.html#memory-layout)
+
+fn get_shared<T>(ptr: &T) -> &UnsafeCell<T> {
+    let t = ptr as *const T as *const UnsafeCell<T>;
+    // SAFETY: `T` and `UnsafeCell<T>` have the same memory layout
+    unsafe { &*t }
+}
+
+// Safety: the caller must ensure that there are no references that
+// point to the *contents* of the `UnsafeCell`.
+unsafe fn get_mut<'a, T>(ptr: &UnsafeCell<T>) -> &'a mut T {
+    unsafe { &mut *ptr.get() }
 }
