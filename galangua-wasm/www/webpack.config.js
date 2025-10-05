9366f0b6e9aac571c147173d0c91784d4f4b2bdc
tyfkda <tyfkda@gmail.com>
2025-10-05 09:05:01 +0900

    Fix web

--- galangua-wasm/www/webpack.config.js
+++ galangua-wasm/www/webpack.config.js
@@ -1,5 +1,6 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin')
 
 module.exports = {
   entry: "./bootstrap.js",
@@ -7,8 +8,40 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
   },
-  mode: "development",
+  mode: "production",
+  experiments: {
+    asyncWebAssembly: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.wasm$/,
+        type: 'webassembly/async',
+      },
+    ],
+  },
   plugins: [
-    new CopyWebpackPlugin(['index.html', 'default.css', 'assets/**/*'])
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'index.html'), to: '' },
+        { from: path.resolve(__dirname, 'default.css'), to: '' },
+        { from: path.resolve(__dirname, 'assets/**/*'), to: '' },
+      ],
+    }),
+    new WasmPackPlugin({
+      crateDirectory: __dirname,
+    }),
   ],
+  performance: {
+    hints: 'warning', // or "error" or false
+    maxAssetSize: 512000, // Increase limit to 500 KB
+    maxEntrypointSize: 512000, // Increase entry point limit
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+  },
+  resolve: {
+    extensions: ['.js', '.wasm'],
+  },
 };
